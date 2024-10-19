14d9708638110ec1450bf879cb00226b271eee07
MohsenHassanHegazy <mhspn.5555@gmail.com>
2024-10-19 12:04:46 +0300

    added invalid user input handler

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,13 +1,39 @@
+import react, { useState } from "react";
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [first, setFirst] = useState(true);
+
+  const inputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const SubmitHandler = (event) => {
+    event.preventDefault();
+    setFirst(false);
+    if (enteredName.trim() === "") {
+      setValidName(false);
+      return;
+    }
+    console.log(enteredName);
+    setEnteredName("");
+    setValidName(true);
+  };
+  const clas = first || validName ? "form-control" : "form-control invalid";
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={SubmitHandler}>
+      <div className={clas}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={inputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
       </div>
+      {!first && !validName && <p className="error-text">name cant be empty</p>}
     </form>
   );
 };
